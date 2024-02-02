268006d8da57bbb920edccc9cd94c5266019d31c
shahzad akram <alyakram786@gmail.com>
2024-02-02 12:01:51 +0500

    updating js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const array = [{
         img: "./tour-1.jpeg",
         title: "Best Of Paris In 7 Days Tour",
         text: "Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. ",
-        text2: "Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich, Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — with cozy villages and alpine vistas all along the way. Join us for the Best of Munich, Salzburg & Vienna in 8 Days!",
+        text2: "Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich, Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — with cozy villages and alpine vistas all along the way. Join us for the Best of Munich, Salzburg & Vienna in 8 Days!",
         Price: "$1,995"
 
     }, {
@@ -16,7 +16,7 @@ const array = [{
         img: "./tour-2.jpeg",
         title: "Best Of Ireland In 14 Days Tour",
         text: "Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites, charming towns, music-filled pubs, and ",
-        text2: "seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moher, the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling city of Belfast. All along the way, Rick's guides will share their stories to draw you in to the Emerald Isle, and the friendliness of the people will surely steal your heart. Join us for the Best of Ireland in 14 Days!",
+        text2: "Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites, charming towns, music-filled pubs, and  seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moher, the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling city of Belfast. All along the way, Rick's guides will share their stories to draw you in to the Emerald Isle, and the friendliness of the people will surely steal your heart. Join us for the Best of Ireland in 14 Days!",
         Price: "$3,895"
 
     },
@@ -24,14 +24,14 @@ const array = [{
         img: "./tour-3.jpeg",
         title: "Best Of Salzburg & Vienna In 8 Days Tour",
         text: "Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. ",
-        text2: "Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich, Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — with cozy villages and alpine vistas all along the way. Join us for the Best of Munich, Salzburg & Vienna in 8 Days!",
+        text2: " Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich, Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — with cozy villages and alpine vistas all along the way. Join us for the Best of Munich, Salzburg & Vienna in 8 Days!",
         Price: "$2,695"
     },
     {
         img: "./tour-4.jpeg",
         title: "Best Of Rome In 7 Days Tour",
         text: "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, ",
-        text2: "your tour guide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica. From the Renaissance and Baroque eras, you'll marvel at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, and Borghese Gallery. You'll also enjoy today's Rome, with neighborhood walking tours, memorable restaurants, and time to explore on your own. Join us for the Best of Rome in 7 Days!",
+        text2: "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica. From the Renaissance and Baroque eras, you'll marvel at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, and Borghese Gallery. You'll also enjoy today's Rome, with neighborhood walking tours, memorable restaurants, and time to explore on your own. Join us for the Best of Rome in 7 Days!",
         Price: "$2,095"
     },
     {
@@ -53,8 +53,8 @@ for (let i = 0; i < array.length; i++) {
     <h4 class="h4">${array[i].Price}</h4>
     <div id = "textArea">
     <h2 class="h2">${array[i].title}</h2>
-    <p class="txt1">${array[i].text}<button id="semore"  class = "see">... see more</button></p>
-    <p class="txt2">${array[i].text2}<button id="seless" class = "see">... see less</button></p>
+    <p class="text" id="txt1">${array[i].text}<button   class = "see semore">... see more</button></p>
+    <p class="text" id="txt2">${array[i].text2}<button class = "see seless">... see less</button></p>
     </div>
     <button class="btn">Not Interested</button>
     </div>
@@ -63,32 +63,46 @@ for (let i = 0; i < array.length; i++) {
 }
 
 // from here i am trying to set button for see more and less
-let text1 = document.querySelectorAll(".txt1");
-let text2 = document.querySelectorAll(".txt2");
+let text1 = document.getElementById("txt1");
+let text2 = document.getElementById("txt2");
+let seemore = document.getElementsByClassName("semore");
+let seeless = document.getElementsByClassName("seless");
 
-let seemore = document.getElementById("semore");
-let seeless = document.getElementById("seless");
+
+
+
 let see = document.querySelectorAll(".see")
+console.log("inside  seee , ", see)
 for (let i = 0; i < see.length; i++) {
     see[i].addEventListener("click", () => {
+        console.log("wrking we are her")
+        let seebtn = document.querySelectorAll('#ins1');
 
-        if (text1 && text2) {
-            // Check if both text1 and text2 exist
-            text1.style.display = text1.style.display === "none" ? "block" : "none";
-            text2.style.display = text2.style.display === "none" ? "block" : "none";
+        if (seebtn) {
+            console.log("inside first if")
+            if (text1 && text2) {
+                console.log("inside 2n if ")
+                    // Check if both text1 and text2 exist
+                    // text1.style.display = text1.style.display === "none" ? "block" : "none";
+                    // text2.style.display = text2.style.display === "none" ? "block" : "none";
 
-            seemore.addEventListener("click", () => {
-                text2.style.display = "block";
-                seemore.style.display = "none";
-                console.log("working")
+                seemore[i].addEventListener("click", () => {
+                    text2[i].style.display = "block";
+                    text1[i].style.display = "none";
+                    seemore.style.display = "none";
+                    console.log("working")
 
-            });
-            seeless.addEventListener("click", () => {
-                text2.style.display = "none";
-                seemore.style.display = "block";
-                console.log("working")
+                });
 
-            });
+                seeless[i].addEventListener("click", () => {
+                    text2[i].style.display = "none";
+                    seemore.style.display = "block";
+                    text1[i].style.display = "block";
+
+                    console.log("working")
+
+                });
+            }
         }
 
     });
@@ -101,31 +115,31 @@ let butn = document.querySelectorAll(".btn");
 for (let i = 0; i < butn.length; i++) {
     butn[i].addEventListener("click", () => {
         console.log("working")
-            // let ins = butn[i].closest('#ins1');
-            // if (ins) {
-            //     ins.remove();
+        let ins = butn[i].closest('#ins1');
+        if (ins) {
+            ins.remove();
 
-        //     if (document.querySelectorAll('#ins1').length === 0) {
-        //         ttl.style.display = "none";
-        //         doc.innerHTML +=
-        //             `
-        //                    <div class="aftr">
-        //                    <h4 id = "title">No Tours Left</h4>
-        //                    <a href="" id="rfrsh"><button >Refresh</button>
-        //                    <a></div>
-        //                 `;
-        // let rfrshbtn = document.getElementById("rfrsh")
-        // rfrshbtn.addEventListener("click", () => {
-        // location.reload();
+            if (document.querySelectorAll('#ins1').length === 0) {
+                ttl.style.display = "none";
+                doc.innerHTML +=
+                    `
+                           <div class="aftr">
+                           <h4 id = "title">No Tours Left</h4>
+                           <a href="" id="rfrsh"><button >Refresh</button>
+                           <a></div>
+                        `;
+                let rfrshbtn = document.getElementById("rfrsh")
+                rfrshbtn.addEventListener("click", () => {
+                    location.reload();
 
 
-    })
+                })
 
-    // }
+            }
 
-    // };
+        };
 
-    // });
+    });
 
 
 
@@ -142,4 +156,15 @@ for (let i = 0; i < butn.length; i++) {
 
 // function updateDisplay() {
 //     doc.innerHTML =
+// }
+
+
+// function str() {
+//     let counnt = 0;
+//     let previous = 0;
+//     for (let i = 0; i < str.length - 1; i++) {
+//         if () {
+
+//         }
+//     }
 // }
